1978a891e3f880c253ac8ee41c73e642a5ee2832
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-13 00:09:12 -0700

    [Charran] Add : Adding Numbers UnitTest

---
+++ AdditionOfTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOfTwoNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int Add(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+    }
+}
